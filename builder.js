c04d06d1db96fc579cd7e30f889533414d4c30f4
Alok Ranjan <aloks@byteridge.com>
2023-05-12 13:59:21 +0530

    added api's

---
+++ builder.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const path = require("path");
+const { promisify } = require("util");
+
+const writeFile = promisify(fs.writeFile);
+
+const characters = require("./api/all.json");
+
+const apiFolderPath = "api";
+
+const rebuild = async () => {
+  await Promise.all(
+    characters.map((c) => {
+      const filepath = path.join(
+        __dirname,
+        apiFolderPath,
+        "id",
+        `${c.id}.json`
+      );
+      return writeFile(filepath, JSON.stringify(c, null, 2));
+    })
+  ).catch(console.error);
+
+  console.log("built successfully");
+};
+
+rebuild();
